64d1d7967f879b840da2bab5e96f81efcd09ae24
whdupreez <willydupreez@gmail.com>
2014-04-24 19:54:03 -0600

    Initial commit.

---
+++ src/main/java/com/willydupreez/prototype/structure/Launcher.java
@@ -0,0 +1,18 @@
+package com.willydupreez.prototype.structure;
+
+/**
+ * The application entry point.
+ *
+ * @author Willy du Preez
+ *
+ */
+public class Launcher {
+
+	public static void main(String[] args) {
+
+		// 1. Handle command-line arguments
+		// 2. Load system properties
+
+	}
+
+}
---
+++ src/test/java/com/willydupreez/prototype/structure/LauncherTest.java
@@ -0,0 +1,12 @@
+package com.willydupreez.prototype.structure;
+
+import org.junit.Test;
+
+public class LauncherTest {
+
+	@Test
+	public void testMain() {
+		Launcher.main(new String[] { });
+	}
+
+}
